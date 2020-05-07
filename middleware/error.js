fcbf041aa941ca07197585f28bd4c866ef9208ed
emmanuel <emmanuel@cbqasolutions.com>
2020-05-07 14:16:23 -0500

    asyncHandler middleware was created

--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,34 @@
+const ErrorResponse = require('../utils/errorResponse');
+
 const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+
+  error.message = err.message;
+
   // Log to console for developer
   console.log(err.stack.red);
 
+  // Mongoose bad ObjectId
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Moongose duplicated key
+  if (err.code === 11000) {
+    const message = 'Duplicated field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose Validation Error
+  if (error.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
   res
-    .status(err.statusCode || 500)
-    .json({ success: false, error: err.message || 'Server Error' });
+    .status(error.statusCode || 500)
+    .json({ success: false, error: error.message || 'Server Error' });
 };
 
 module.exports = errorHandler;
