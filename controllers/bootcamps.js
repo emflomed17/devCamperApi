c6c4e5a3bd3bae7c008b64c93f44a3bd83ad260f
emmanuel <emmanuel@cbqasolutions.com>
2020-05-07 12:40:08 -0500

    Custom ErrorResponse Class was created

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc Get all bootcamps
@@ -23,13 +24,21 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      // If is a formatted id but does not exist in the db
+      return next(
+        new ErrorResponse(
+          `$Bootcamp not found with id of ${req.params.id}`,
+          404
+        )
+      );
     }
 
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    // res.status(400).json({ success: false });
-    next(err);
+    // If is not a formatted id
+    next(
+      new ErrorResponse(`$Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
 };
 
